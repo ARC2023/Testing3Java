cd4109fe70ba8563e992d644c7da9f56d3c836f2
ARC2023 <arcautomation2023@gmail.com>
2023-04-20 10:59:29 +0530

    Conditonal Class Push

---
+++ src/com/qa/java/conditonal/ConditionalProgramming.java
@@ -0,0 +1,59 @@
+package com.qa.java.conditonal;
+
+public class ConditionalProgramming {
+
+	public static void main(String[] args) {
+		int balanceOnScreen=25000;
+		  int balanceFromDB=26000;
+		  
+		
+		if(balanceOnScreen==balanceFromDB && balanceOnScreen>balanceFromDB) {
+			
+			System.out.println("Inside If Block");
+			
+			
+		} else if(balanceOnScreen>balanceFromDB){
+			
+			
+			
+			if(22==22) {
+			}
+			System.out.println("Inside  Else If Block");
+			
+		}else if(balanceOnScreen>balanceFromDB){
+			System.out.println("Inside  Else If Block");
+			
+		}else if(balanceOnScreen>balanceFromDB){
+			System.out.println("Inside  Else If Block");
+			
+		}else if(balanceOnScreen>balanceFromDB){
+			System.out.println("Inside  Else If Block");
+			
+		}else if(balanceOnScreen>balanceFromDB){
+			System.out.println("Inside  Else If Block");
+			
+		}else if(balanceOnScreen>balanceFromDB){
+			System.out.println("Inside  Else If Block");
+			
+		}
+		
+		
+		int pin=1290;
+		int amount =9000;
+
+		if(pin==1290 && amount==9000) {
+			
+			System.out.println("Withdraw Cash");
+			
+			
+		} else {
+			System.out.println("Inside  Else If Block");
+			
+		}
+		
+		
+		
+
+	}
+
+}
